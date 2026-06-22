544c87fcda237c02d1046b20df8c1768a2d0832b
Sagar Bhandari <rsaagar7200@gmail.com>
2026-06-22 13:56:41 +0545

    add: data typs

---
+++ 2.datatypes.js
@@ -0,0 +1,47 @@
+//! datatypes
+
+//? primitive
+//* number
+let a = 134;
+let b = 45.67;
+
+//* string
+let c = "sjngkfd";
+// let d = '';
+
+//? template literal -> ``
+let e = `djfkjds
+sdfdsjsdfs
+sdnsd`;
+
+let d = "John";
+
+// let f = `Hello ${2 + 2}`; //? Hello 4
+let f = `Hello ${d}`; //? Hello John
+
+console.log(f);
+
+//* boolean
+let g = true;
+let h = false;
+
+//* undefined
+let i;
+console.log(i); //
+
+//* null
+let j = null;
+
+//? diff betwn undefined & not defined
+let l;
+console.log(l); // undefined
+
+console.log(k); // ReferenceError: k is not defined
+
+//? non primitive
+
+//* object
+//* array
+//* function
+
+//todo: operators
