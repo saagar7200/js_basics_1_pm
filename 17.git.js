ac832d10461c6005d4438965c322e8c92186432c
Sagar Bhandari <rsaagar7200@gmail.com>
2026-06-24 13:13:42 +0545

    checkout commands

--- 17.git.js
+++ 17.git.js
@@ -33,13 +33,14 @@
 //? remote
 //* git remote -v
 //* git remote add origin <remote_url>
+//* git remote remove origin
 
 //todo: branch
 //* git branch -> list all local branches
 //* git branch <branch_name>    -> create new branch
 //* git branch -d <branch_name>  -> delete local branch
-//* git switch <branch_name>    -> switch between branch
-//* git switch -c <branch_name> -> create & switch branch
+//* git switch <branch_name>    -> switch between branch   // git checkout <branch_name>
+//* git switch -c <branch_name> -> create & switch branch  // git checkout -b <branch_name>
 
 //* git merge <branch_name>
 
